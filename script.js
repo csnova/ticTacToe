f594de67aff446298ade2ae57af30b37d85f78b2
csnova <chandlersnova@gmail.com>
2023-09-01 14:58:37 -0700

    Added some Logic Tried to push the array to display on the board but has some bugs

--- script.js
+++ script.js
@@ -5,37 +5,34 @@ const Player = (name, piece) => {
 };
 
 const gameBoard = (() => {
-  const a1 = (a) => "X";
-  const a2 = (a) => "O";
-  const a3 = (a) => "X";
-  const b1 = (a) => "X";
-  const b2 = (a) => "X";
-  const b3 = (a) => "O";
-  const c1 = (a) => "O";
-  const c2 = (a) => "X";
-  const c3 = (a) => "O";
+  const board = [
+    ["X", "O", "X"],
+    ["0", "X", "O"],
+    ["X", "O", "X"],
+  ];
   return {
-    a1,
-    a2,
-    a3,
-    b1,
-    b2,
-    b3,
-    c1,
-    c2,
-    c3,
+    board,
   };
 })();
 
 const displayController = (() => {
-  const a1 = "X";
-  const a2 = "X";
-  return {
-    a1,
-    a2,
-  };
+  let i = 0;
+  for (const row of gameBoard.board) {
+    let j = 0;
+    for (const tile of row) {
+      const board = document.querySelector(`.board`);
+      const row = document.querySelector(`.row-${i}`);
+      const column = document.querySelector(`.column-${j}`);
+      const tile = document.createElement("p");
+      tile.textContent = gameBoard.board[i][j];
+      column.appendChild(tile);
+      console.log(i, j);
+      console.log(gameBoard.board[i][j]);
+      j++;
+    }
+    i++;
+  }
 })();
 
-console.log(
-  `${gameBoard.a1}, ${gameBoard.a2}, ${gameBoard.a3}, ${gameBoard.b1}, ${gameBoard.b2}, ${gameBoard.b3}, ${gameBoard.c1}, ${gameBoard.c2}, ${gameBoard.c3}`
-);
+displayController;
+// console.table(gameBoard.board[1][2]);
